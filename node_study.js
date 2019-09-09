e3e7e5bea3f380252a2eed50b873880c8b141851
doohong <wnghd94@gmail.com>
2019-09-10 00:53:11 +0900

    express

--- node_study.js
+++ node_study.js
@@ -14,13 +14,29 @@
 // }).listen(8081);
 
 
-var express = require("express");
-var app = express();
-var server = app.listen(3000, function(){
-    console.log("Express server has started on port 3000");
-})
-// Cannot GET 
+// var express = require("express");
+// var app = express();
+// var server = app.listen(3000, function(){
+//     console.log("Express server has started on port 3000");
+// })
+// // Cannot GET 
 
-app.get('/',function(req,res){
-    res.send("hello World");
-})
+// app.get('/',function(req,res){
+//     res.send("hello World");
+// })
+// // 기본 라우터
+
+var express = require('express');
+var app = express();
+var router = require('./router')(app);
+
+// view 경로 설정
+app.set('views', __dirname + '/');
+
+// 화면 engine을 ejs로 설정
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+var server = app.listen(3000, function(){
+    console.log("Express server has started on port 3000")
+});
